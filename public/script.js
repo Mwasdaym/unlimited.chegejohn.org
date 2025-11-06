a7ff0c31f74a21579c1ee5ac02ed777a9f28ffac
Mwasdaym <johnchege2159@gmail.com>
2025-11-06 06:30:32 -0900

    Update script.js

--- public/script.js
+++ public/script.js
@@ -673,4 +673,12 @@ document.addEventListener('submit', function(e) {
             }
         }
     }
+    function toggleDarkMode() {
+  document.body.classList.toggle("dark-mode");
+
+  // Save preference
+  const mode = document.body.classList.contains("dark-mode") ? "dark" : "light";
+  localStorage.setItem("theme", mode);
+}
+}
 });
